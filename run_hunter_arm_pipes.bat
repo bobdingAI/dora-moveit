e8ccc62b7aad9b1794b2ed8a94b08725dbb7a625
bobd988 <dingxinmin.bob@gmail.com>
2026-02-22 19:52:27 -0800

    Remove leftover upstream hunter/car/vehicle files and __pycache__
    Clean up files from upstream/main that are not part of the LM3 branch:
    - ArmWithCar.gif, hunter configs, vehicle controller, test files
    - All tracked __pycache__ directories

--- run_hunter_arm_pipes.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Hunter SE with GEN72 Arm and Industrial Pipes Scene...
-
-start /B cmd /c "dora up"
-timeout /t 5 /nobreak >nul
-
-dora build config/dataflow_hunter_arm_mujoco.yml
-dora start config/dataflow_hunter_arm_mujoco.yml
